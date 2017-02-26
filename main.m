f5cdbc231da733b481f8266822c1602d6384a569
Deepali Kamat <deepalidkamat@gmail.com>
2017-02-26 13:58:08 -0500

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,3 @@
-Ball_Count('/Users/deepaliKamat/Documents/MATLAB/Balls_01.jpg');
-Ball_Count('/Users/deepaliKamat/Documents/MATLAB/Balls_02.jpg’);
-Find_Rotation('/Users/deepaliKamat/Documents/MATLAB/image_in.jpg');
+Ball_Count('Balls_01.jpg');
+Ball_Count('Balls_02.jpg’);
+Find_Rotation('image_in.jpg');
